5031937592158e9ba584b384823c8669efff237f
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-06-26 20:08:03 +0530

    Deploy project

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const authRoute = require("./routes/auth.route");
 const messageRoute = require("./routes/message.route");
+const path = require("path");
 
 const app = express();
 
+
+
 app.use("/api/auth",authRoute);
 app.use("/api/messages", messageRoute);
+if(process.env.NODE_ENV=== "production"){
+    app.use(express.static(path.join(__dirname,"../../frontend/dist")));
+    app.get(/.*/, (req,res)=>{
+        res.sendFile(path.join(__dirname,"../../frontend/dist/index.html"))
+    })
+}
 
 
 
