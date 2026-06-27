245f8079dddb150a923034a1a955b9e150718e3b
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-06-27 13:23:15 +0530

    login & logout controller added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -56,8 +56,43 @@ async function signup(req,res){
     
 }
 
+async function login (req,res){
+    const {email,password} = req.body;
+    const User = await user.findOne({email});
+    if (!User) return res.status(400).json({message:"Invalid Credentials"});
+    else{
+        const isPasswordMatched = await bcrypt.compare(password, User.password);
+        if(! isPasswordMatched) return res.status(400).json({message:"Invalid Credentials"});
+        const token = generateToken(User._id);
+        res.cookie("uid",token,{
+            maxAge : 7*24*60*60*1000,
+            httpOnly: true,         
+            secure:process.env.NODE_ENV==="production",       
+            sameSite: "strict"
+        });
+        return res.status(201).json({
+            _id : newUser._id,
+            email : newUser.email,
+            profilePic : newUser.profilePic,
+        })
+        
+    }
+
+}
+
+async function logout (req,res){
+    res.clearCookie("uid", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+    });
+    return res.status(200).json({ message: "Logged out successfully" });
+
+}
 
 
 module.exports = {
     signup,
+    login,
+    logout,
 }
