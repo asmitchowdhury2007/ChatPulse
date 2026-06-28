58895f46d475b68020b0dc6b066bd30acb94168c
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-06-29 00:05:02 +0530

    frontend setup done

---
+++ frontend/src/Pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AuthLayout from "../components/AuthLayout.jsx"
+
+function LoginPage() {
+  return (
+    <div>
+      <AuthLayout>
+      
+      </AuthLayout>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ frontend/src/Pages/SignUpPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AuthLayout from "../components/AuthLayout.jsx"
+
+function SignUpPage() {
+  return (
+    <div>
+      <AuthLayout>
+      
+      </AuthLayout>
+    </div>
+  )
+}
+
+export default SignUpPage
