d9771821fbbf7a1c5862be1184d4d63542bda66d
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-07-07 00:50:55 +0530

    Minor Fixes

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import message from "../models/message.js"
 import user from "../models/user.js"
 import cloudinary from"../lib/cloudinary.js"
-import {getReceiverSocketId} from "../lib/socket.js"
+import {io,getReceiverSocketId} from "../lib/socket.js"
 
 async function getAllContacts(req,res){
     const loggedInUserID = req.user._id;
