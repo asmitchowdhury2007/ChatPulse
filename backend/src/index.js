2c6b516655edbb152417de5b9d265b3339cd8e6f
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-07-13 22:53:33 +0530

    Remove frontend static serving for Render deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,12 +23,12 @@ app.use(cors({origin: process.env.CLIENT_URL,credentials: true,}));
 
 app.use("/api/auth",authRoute);
 app.use("/api/messages", messageRoute);
-if(process.env.NODE_ENV=== "production"){
-    app.use(express.static(path.join(__dirname,"../../frontend/dist")));
-    app.get(/.*/, (req,res)=>{
-        res.sendFile(path.join(__dirname,"../../frontend/dist/index.html"))
-    })
-}
+// if(process.env.NODE_ENV=== "production"){
+ //   app.use(express.static(path.join(__dirname,"../../frontend/dist")));
+ //   app.get(/.*/, (req,res)=>{
+  //      res.sendFile(path.join(__dirname,"../../frontend/dist/index.html"))
+  //  })
+//}
 
 
 
