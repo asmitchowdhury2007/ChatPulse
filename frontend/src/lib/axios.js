4e13f913dd9768dfbd1b833f5372999cfb22757c
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-07-14 00:45:36 +0530

    Fix frontend API configuration for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL : import.meta.env.MODE === "development"? "http://localhost:9000": import.meta.env.VITE_API_URL,
+    baseURL : import.meta.env.MODE === "development"? "http://localhost:9000/api": import.meta.env.VITE_API_URL,
     withCredentials: true, 
 })
 
